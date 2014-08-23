56940e98b49339d466a352787b0eb59e5cbb6d7f
Dan Herbst <nr5go@yahoo.com>
2014-08-23 16:17:17 -0400

    Added jQuery masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Using devise with heroku
   config.action_mailer.default_url_options = { :host => 'http://nr5-pinteresting.herokuapp.com/' }
-  
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
